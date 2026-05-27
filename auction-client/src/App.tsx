aae457125229cf138ea754eec5605754fe55becb
sweviceroy <113086422+sweviceroy@users.noreply.github.com>
2026-05-27 15:43:20 +0200

    Wire up App routing, global styles, and replace template boilerplate

--- auction-client/src/App.tsx
+++ auction-client/src/App.tsx
@@ -1,5 +1,35 @@
-function App() {
-  return <h1>Auction App</h1>
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import AuctionDetail from './pages/AuctionDetail';
+import CreateAuction from './pages/CreateAuction';
 
-export default App
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/auctions/:id" element={<AuctionDetail />} />
+            <Route
+              path="/auctions/create"
+              element={
+                <ProtectedRoute>
+                  <CreateAuction />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </main>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
